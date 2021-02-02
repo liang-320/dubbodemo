2d465c99d956b96751f4b83f6f3e3dbed7e20d24
梁振江 <805863021@qq.com>
2021-02-02 16:10:54 +0800

    test01 master

---
+++ dubbodemo_consumer/src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @Author Zhenjiang Liang
+ * @Date 2021/2/2 16:09
+ * @Version 1.0
+ */
+public class Test1 {
+    @Test
+    public void test01(){
+        System.out.println("11111");
+    }
+}
